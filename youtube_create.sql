f8d25070dacf6b7b7bae108077e61f6e6d2d2821
Joseph Tibog <jtibo002@ucr.edu>
2020-11-04 20:30:56 -0800

    Update youtube_create.sql

--- youtube_create.sql
+++ youtube_create.sql
@@ -21,10 +21,6 @@ CREATE TABLE Account
         comment_id INTEGER NOT NULL,
         subcount INTEGER NOT NULL,
         num_videos INTEGER NOT NULL,
-        --video CHAR(100) NOT NULL, --changed to num_videos on pgadmin
-        --history CHAR(100) NOT NULL,
-        --recommended CHAR(100) NOT NULL,
-        --subscriptions CHAR(256) NOT NULL,
         PRIMARY KEY (id)
 );
 
