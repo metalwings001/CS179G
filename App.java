2f05289cf8f6333cca0fb79cf9af7c74732adbd3
Justin Deleon <jdele013@ucr.edu>
2020-12-02 14:54:22 -0800

    added execution of hadoop commands within java using shell

--- App.java
+++ App.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.io.IOException;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author postgresqltutorial.com
@@ -57,12 +60,58 @@ public class App{
             System.err.println( e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
         }       
-      String command = "C:\\Program Files (x86)\\Windows Media Player\\wmplayer.exe";
+      /*String command = "C:\\Program Files (x86)\\Windows Media Player\\wmplayer.exe";
       String arg = "C:\\Users\\Justin\\Desktop\\data\\mp4\\dog.mp4";
       //Building a process
       ProcessBuilder builder = new ProcessBuilder(command, arg);
       System.out.println("Executing the external program . . . . . . . .");
-      //Starting the processs
-      builder.start();
+      //Starting the process
+      builder.start();*/
+      
+    Process p;
+    try {
+      //p = Runtime.getRuntime().exec("cmd /c hdfs dfs -get /sample/test.txt C:\\Users\\Justin\\Desktop\\fromHDFS");
+      p = Runtime.getRuntime().exec("cmd /c hdfs dfs -get /sample/dog.mp4 C:\\Users\\Justin\\Desktop\\fromHDFS");
+
+      p.waitFor(); 
+      BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+      String line; 
+      while((line = reader.readLine()) != null) { 
+        System.out.println(line);
+      }
     }
+    catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } 
+    catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    
+    /* try {  
+            String shpath="cmd help";
+            Process ps = Runtime.getRuntime().exec(shpath); 
+            String line;
+            StringBuilder output = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader (ps.getInputStream()));
+            while((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+            int exitVal = ps.waitFor();
+            if(exitVal == 0) {
+                    System.out.println("Success");
+                    System.out.println(output);
+                    System.exit(0);
+            
+            }  
+            else{
+                System.out.println("error\n");
+            }
+      }
+     catch (Exception e) {  
+          e.printStackTrace();  
+      }  
+    }*/
+}
 }
